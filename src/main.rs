ed629fd1920c012515d92c4b0f62d5d18abe21cf
pefontana <pedro.fontana@lambdaclass.com>
2022-11-16 01:17:31 -0300

    serialize enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{fs::File, io::Write};
+
+use serde::Serialize;
+
+
+#[derive(Debug, Serialize)]
+enum Move {
+  Up,
+  Down,
+  Right,
+  Left,  
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let a = Move::Up;
+
+    let json = serde_json::to_string(&a).unwrap();
+
+    let mut file = File::create("serde.txt").unwrap();
+    file.write_all(json.as_bytes()).unwrap();
+
 }
